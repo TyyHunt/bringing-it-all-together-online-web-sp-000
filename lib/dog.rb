c21807665d2a0a0c8d9f216b246b4a33da3d1615
Tyler Hunt <tyhunt04@gmail.com>
2019-03-18 04:38:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -69,7 +69,8 @@ end
 
   def self.find_by_name(name)
     sql = "SELECT * FROM dogs WHERE name = ?"
-    DB[:conn].execute(sql, name)
+    row = DB[:conn].execute(sql, name)
+    self.new(id: row[0][0], name: row[0][1], breed: row[0][2])
   end
 
   def update
